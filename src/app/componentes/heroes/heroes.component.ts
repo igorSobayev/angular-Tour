de1f0178a70f3a929f71bf940384c75e5cbb6df0
igorSobayev <isobayev@gmail.com>
2019-03-20 09:57:03 +0100

    Añado un array con todos los heroes, muestro el listado de todos los heroes añadido en la plantilla de los heroes y añado estilos

--- src/app/componentes/heroes/heroes.component.ts
+++ src/app/componentes/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { HEROES } from './../../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +9,19 @@ import { Hero } from './hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero:Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+  // array con los heroes
+  heroes = HEROES;
+  // heroe vacio por defecto pero que se va a llenar cuando sea seleccionado uno
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  // método de asignación de un heroe
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
