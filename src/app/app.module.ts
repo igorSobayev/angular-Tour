7827cb732dc0e268f22b73c828dcfd832b2e38c2
igorSobayev <isobayev@gmail.com>
2019-03-23 18:49:39 +0100

    Proyecto finalizado, solo queda ajustar los estilos de los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './servicios/in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +11,7 @@ import { HeroesComponent } from './componentes/heroes/heroes.component';
 import { HeroDetailComponent } from './componentes/hero-detail/hero-detail.component';
 import { MessagesComponent } from './componentes/messages/messages.component';
 import { DashboardComponent } from './componentes/dashboard/dashboard.component';
+import { HeroSearchComponent } from './componentes/hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +19,18 @@ import { DashboardComponent } from './componentes/dashboard/dashboard.component'
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
